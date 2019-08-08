1919e23c1b34e3c887a957481eaa4ca5be6ef2d9
Ulibka <gena68@inbox.ru>
2019-08-08 15:54:30 +0300

    14. Run Client Script with Server, Add Markup for Brands Section

--- ecommerce-react-graphql-stripe/client/src/components/App.js
+++ ecommerce-react-graphql-stripe/client/src/components/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import { Container, Box, Heading } from "gestalt";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <Container>
+        {/* Brands Section */}
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          {/* Brands Header */}
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
+    );
   }
 }
 
