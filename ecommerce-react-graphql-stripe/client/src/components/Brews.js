1ac3b0f6695c63e07a809ca80fe8c88b7c050eac
Ulibka <gena68@inbox.ru>
2019-08-09 16:06:55 +0300

    32. Create Sign Up Form

--- ecommerce-react-graphql-stripe/client/src/components/Brews.js
+++ ecommerce-react-graphql-stripe/client/src/components/Brews.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Strapi from "strapi-sdk-javascript/build/main";
-import { Box, Heading, Text, Image, Card, Button, Mask } from "gestalt";
+// prettier-ignore
+import { Box, Heading, Text, Image, Card, Button, Mask, IconButton } from "gestalt";
+import { calculatePrice, setCart, getCart } from "../utils";
 import { Link } from "react-router-dom";
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
@@ -35,12 +37,38 @@ class Brews extends React.Component {
       });
       this.setState({
         brews: response.data.brand.brews,
-        brand: response.data.brand.name
+        brand: response.data.brand.name,
+        cartItems: getCart()
       });
     } catch (err) {
       console.error(err);
     }
   }
+
+  addToCart = brew => {
+    const alreadyInCart = this.state.cartItems.findIndex(
+      item => item._id === brew._id
+    );
+
+    if (alreadyInCart === -1) {
+      const updatedItems = this.state.cartItems.concat({
+        ...brew,
+        quantity: 1
+      });
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
+    } else {
+      const updatedItems = [...this.state.cartItems];
+      updatedItems[alreadyInCart].quantity += 1;
+      this.setState({ cartItems: updatedItems }, () => setCart(updatedItems));
+    }
+  };
+
+  deleteItemFromCart = itemToDeleteId => {
+    const filteredItems = this.state.cartItems.filter(
+      item => item._id !== itemToDeleteId
+    );
+    this.setState({ cartItems: filteredItems }, () => setCart(filteredItems));
+  };
 
   render() {
     const { brand, brews, cartItems } = this.state;
@@ -106,7 +134,11 @@ class Brews extends React.Component {
                     <Text color="orchid">${brew.price}</Text>
                     <Box marginTop={2}>
                       <Text bold size="xl">
-                        <Button color="blue" text="Add to Cart" />
+                        <Button
+                          onClick={() => this.addToCart(brew)}
+                          color="blue"
+                          text="Add to Cart"
+                        />
                       </Text>
                     </Box>
                   </Box>
@@ -126,14 +158,29 @@ class Brews extends React.Component {
               padding={2}
             >
               {/* User Cart Heading */}
-              <Heading align="center" size="md">
+              <Heading align="center" size="sm">
                 Your Cart
               </Heading>
               <Text color="gray" italic>
                 {cartItems.length} items selected
               </Text>
 
-              {/* Cart Items (will add) */}
+              {/* Cart Items */}
+              {cartItems.map(item => (
+                <Box key={item._id} display="flex" alignItems="center">
+                  <Text>
+                    {item.name} x {item.quantity} - $
+                    {(item.quantity * item.price).toFixed(2)}
+                  </Text>
+                  <IconButton
+                    accessibilityLabel="Delete Item"
+                    icon="cancel"
+                    size="sm"
+                    iconColor="red"
+                    onClick={() => this.deleteItemFromCart(item._id)}
+                  />
+                </Box>
+              ))}
 
               <Box
                 display="flex"
@@ -146,7 +193,7 @@ class Brews extends React.Component {
                     <Text color="red">Please select some items</Text>
                   )}
                 </Box>
-                <Text size="lg">Total: $3.99</Text>
+                <Text size="lg">Total: {calculatePrice(cartItems)}</Text>
                 <Text>
                   <Link to="/checkout">Checkout</Link>
                 </Text>
--- ecommerce-react-graphql-stripe/client/src/components/Signup.js
+++ ecommerce-react-graphql-stripe/client/src/components/Signup.js
@@ -1,8 +1,82 @@
 import React from "react";
+import { Container, Box, Button, Heading, Text, TextField } from "gestalt";
 
 class Signup extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  handleChange = ({ event, value }) => {
+    event.persist();
+    this.setState({ [event.target.name]: value });
+  };
+
   render() {
-    return <div>Signup</div>;
+    return (
+      <Container>
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: "#ebe2da"
+            }
+          }}
+          margin={4}
+          padding={4}
+          shape="rounded"
+          display="flex"
+          justifyContent="center"
+        >
+          {/* Sign Up Form */}
+          <form
+            style={{
+              display: "inlineBlock",
+              textAlign: "center",
+              maxWidth: 450
+            }}
+          >
+            {/* Sign Up Form Heading */}
+            <Box
+              marginBottom={2}
+              display="flex"
+              direction="column"
+              alignItems="center"
+            >
+              <Heading color="midnight">Let's Get Started</Heading>
+              <Text italic color="orchid">
+                Sign up to order some brews!
+              </Text>
+            </Box>
+            {/* Username Input */}
+            <TextField
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+            {/* Email Address Input */}
+            <TextField
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email Address"
+              onChange={this.handleChange}
+            />
+            {/* Password Input */}
+            <TextField
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+            <Button inline color="blue" text="Submit" type="submit" />
+          </form>
+        </Box>
+      </Container>
+    );
   }
 }
 
